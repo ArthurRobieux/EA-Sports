2c23f01b49b1e562eaf6fdf82f67ab07d9e6f51a
Arthur <arthur.robieux@gmail.com>
2019-08-27 14:30:52 +0200

    Create game modal

--- src/modules/common-ui/Modal/Modal.tsx
+++ src/modules/common-ui/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactModal from "react-modal";
 import styles from "./styles.module.scss";
+import Fifa20Background from "../../../assets/img/Fifa20Background.jpg";
 
 export type ModalProps = {
   children: any;
@@ -21,10 +22,14 @@ export const Modal = ({
       bottom: "auto",
       marginRight: "-50%",
       transform: "translate(-50%, -50%)",
-      width: window.innerWidth <= 768 ? "80%" : "50%",
+      width: window.innerWidth <= 768 ? "80%" : "40%",
       overflow: "visible",
       background: "gray",
-      border: "none"
+      border: "none",
+      backgroundImage: `url(${Fifa20Background})`,
+      backgroundSize: "cover",
+      backgroundPosition: "50% 0%",
+      backgroundRepeat: "no-repeat"
     },
     overlay: { zIndex: "10000000", backgroundColor: "rgba(0, 0, 0, 0.25)" }
   };
--- src/modules/quiz/containers/Step3/ModalForm/ModalForm.tsx
+++ src/modules/quiz/containers/Step3/ModalForm/ModalForm.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-// import styles from "./styles.module.scss";
+import styles from "./styles.module.scss";
 import { Button } from "../../../../common-ui/Button";
 import { TextInput } from "../../../../common-ui/TextInput";
 import { useTranslation } from "react-i18next";
+import { Checkbox } from "../../../../common-ui/Checkbox";
 
 export type ModalFormProps = {
   setModalIsOpen: (b: boolean) => void;
@@ -12,6 +13,7 @@ export const ModalForm = ({ setModalIsOpen }: ModalFormProps) => {
   const [t] = useTranslation();
   const [form, setForm] = useState({ email: "" });
   const [formErrors, setFormErrors] = useState({ email: "" });
+  const [checked, setChecked] = useState(false);
 
   const submit = () => {
     const emailFormat = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,30}\.[a-z]{2,4}$/;
@@ -32,7 +34,14 @@ export const ModalForm = ({ setModalIsOpen }: ModalFormProps) => {
   };
 
   return (
-    <div>
+    <div className={styles.content}>
+      <img
+        src={require("../../../../../assets/img/PlayersWithLogo.png")}
+        className={styles.players}
+      />
+      <div className={styles.title}>
+        {t("Inscris-toi au tirage au sort et tente de gagner un jeu FIFA 20")}
+      </div>
       <TextInput
         value={form.email}
         onChange={evt => setForm({ ...form, email: evt.target.value })}
@@ -41,8 +50,21 @@ export const ModalForm = ({ setModalIsOpen }: ModalFormProps) => {
         error={formErrors.email}
         type={"text"}
       />
-      <Button description={t("Envoyer")} onClick={() => submit()} />
-      <Button description={t("Annuler")} onClick={() => setModalIsOpen(false)} />
+      <div className={styles.conditions} onClick={() => setChecked(!checked)}>
+        <Checkbox
+          onChange={() => setChecked(!checked)}
+          checked={checked}
+          color={"#fff"}
+        />
+        {t(
+          "J'accepte que mon adresse email soit utilisée pour le tirage au sort de ce jeu-concours"
+        )}
+      </div>
+      <Button
+        description={t("Je m'inscris")}
+        onClick={() => submit()}
+        disabled={!checked}
+      />
     </div>
   );
 };
